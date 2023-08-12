197eccf681a13517c1cff0e046ffbca0d7940d4e
Isaranwa <pennyisaranwa@gmail.com>
2023-08-12 21:11:37 +0300

    Add function playGame

--- game.js
+++ game.js
@@ -1,45 +1,83 @@
+const playerText =document.querySelector("#player");
+const computerText = document.querySelector("#computer");
+const resultText = document.querySelector("#result");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+const scoreView = document.querySelector("#score");
+
+
+let player;
+let computer;
+let playerScore = 0;
+let computerScore = 0;
+let rounds = 5;
+ 
+
+function playRound(player,computer){
+    for(i=0;i <= 5;i++){
+    if ( computer === player && rounds< 5 ){
+        return  "It is a draw!";
+    }else if(computer==="rock"&& player==="paper"){
+        playerScore++;
+        console.log(playerScore);
+        return ("You win! Computer looses!");
+    }else if(computer==="paper"&& player==="scissors"){
+        playerScore++;
+        console.log(playerScore);
+        return ("You win! Computer looses!");
+    }else if(computer==="scissors"&& player==="rock"){
+        playerScore++;
+        console.log(playerScore);
+        return ("You win! Computer looses!" );
+    }else {
+        computerScore++ ;
+        console.log(computerScore);
+        return (`Computer wins! You Loose`);
+    }
+
+}
+}
+
+
+
+
+
 function computerChoice(){
-    let choice = (Math.floor(Math.random()*3));
-    if (choice === 0){
-        return "rock"
-    }else if (choice === 1){
-        return "paper"
-    }else if(choice === 2){
-        return "scissors"
+    let choice = (Math.floor(Math.random()*3)+1);
+
+    switch(choice){
+        case 1:
+        computer ="rock";
+        break;
+        case 2:
+            computer ="paper";
+            break;
+        case 3:
+            computer ="scissors";
+            break;
     }
 }
-console.log(computerChoice());
 
-let player = prompt("Enter your choice");
-let playerSelection = player.toLowerCase();
-let computerSelection = computerChoice();
+choiceBtns.forEach(button=>button.addEventListener("click",()=>
+{
+    player=button.textContent;
+    computerChoice();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = playRound(player,computer);
+    scoreView.textContent = `0${playerScore}:0${computerScore}`
+    
+}));
 
- function playRound(playerSelection,computerSelection){
-        if ( computerSelection === playerSelection){
-            console.log( "It is a draw!");
-        }
-            else if ((computerSelection==="rock" && playerSelection==="paper")){
-                console.log("Player wins!");
-            }
-                else if((computerSelection ==="paper")&&(playerSelection=== "scissors")){
-                    console.log ("Player Wins!");
-                }
-            else if((computerSelection ==="paper")&&(playerSelection=== "scissors")){
-                console.log ("Player Wins!");
-            }
-                else if ((computerSelection==="scissors")&&(playerSelection==="paper")){
-                    console.log ("Computer Wins!");
-                }
 
-            else if((computerSelection ==="scissors")&&(playerSelection=== "rock")){
-                    console.log ("Player Wins!");}  
-                    else if ((computerSelection==="rock")&&playerSelection==="scissors"){
-                        console.log("Computer wins");
-                    }                        
- }
+ 
 
-console.log(playRound(playerSelection,computerSelection));
 
+
+
+ 
+
+ 
+ 
 
 
     
